99b76dc100a7d5f400db2694bf4fbfa34565e18c
Giri-Shankar <jrironman196@gmail.com>
2025-11-22 23:11:09 +0530

    homepage and base url changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // Set base to your repository name so built asset URLs include the subpath.
 // This makes the built index.html reference /ai-analytics-page/assets/... instead of /assets/...
 export default defineConfig({
-  base: '/ai-analytics-page/',
+  base: '/ai-analytics-v2/',
   plugins: [react()],
 })
